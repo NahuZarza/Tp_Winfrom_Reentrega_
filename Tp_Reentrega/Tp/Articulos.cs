1db3c12d80ce08f7466f25a12035eabe3cba5cfe
NahuZarza <nahuelalcala627@gmail.com>
2025-09-26 13:48:34 -0300

    Agregue el archivo de BD que era y, ademas, agregue nuevos archivos

---
+++ Tp_Reentrega/Tp/Articulos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp
+{
+    internal class Articulos
+    {
+        public int CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public int IdCategoria { get; set; }
+        public int IdMarca { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
