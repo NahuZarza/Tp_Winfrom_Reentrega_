71a1514ec2e3fe00bb664c78c5db4d135651805f
NahuZarza <nahuelalcala627@gmail.com>
2025-09-26 12:58:53 -0300

    Agregue las tablas de categoria, Marcas y Articulos

--- TpWinfrom.sql
+++ TpWinfrom.sql
@@ -1,3 +1,31 @@
 Create Database Tp_WinForms;
 Go
 Use Tp_WinForms;
+go
+
+Create Table Categorias(
+    IdCategoria int PRIMARY KEY IDENTITY(1,1),
+    Nombre varchar(100) NOT NULL,
+)
+go
+
+Create Table Marcas(
+    IdMarca int PRIMARY KEY IDENTITY(1,1),
+    Nombre varchar(100) NOT NULL,
+)
+
+go
+
+Create Table Articulos(
+    CodigoArticulo int PRIMARY KEY IDENTITY(1,1),
+    Nombre varchar(100) NOT NULL,
+    Descripcion varchar(255) NOT NULL,
+    IdCategoria int NOT NULL,
+    IdMarca int NOT NULL,
+    ImagenUrl varchar(255) NOT NULL,
+    Precio decimal(10,2) NOT NULL,
+
+    FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
+    FOREIGN KEY (IdMarca) REFERENCES Marcas(IdMarca),
+
+)
